4a1090ab4d333d19855fd0efda0175df40f8d7eb
Estefane1992 <108195915+Estefane1992@users.noreply.github.com>
2023-05-03 11:59:19 -0300

    Add files via upload
    projeto concluído

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
+
 
 
 @NgModule({
@@ -23,6 +28,10 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HomeComponent,
     NewMomentComponent,
     MomentFormComponent,
+    MessagesComponent,
+    MomentComponent,
+    EditMomentComponent,
+
     
   ],
   imports: [
@@ -31,6 +40,7 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    FontAwesomeModule,
 
   ],
   providers: [],
